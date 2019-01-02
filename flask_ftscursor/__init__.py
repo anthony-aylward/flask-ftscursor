8bf21593ecc708d32aa64ca181d4f8c321f00940
anthony-aylward <aaylward@eng.ucsd.edu>
2019-01-02 15:36:06 -0800

    update

--- flask_ftscursor/__init__.py
+++ flask_ftscursor/__init__.py
@@ -1,2 +1,10 @@
-from flask_ftscursor.flask_ftscursor import FTS, index, delete, search,
-search_results, add_to_index, remove_from_index, query_index
+from flask_ftscursor.flask_ftscursor import (
+  FTS,
+  fts_index,
+  fts_delete,
+  fts_search,
+  search_results,
+  add_to_index,
+  remove_from_index,
+  query_index
+)
