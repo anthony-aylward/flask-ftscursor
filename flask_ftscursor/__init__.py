2391f705e291d8c918cfa79e902b9e5a58ab47bd
anthony-aylward <aaylward@eng.ucsd.edu>
2019-01-02 13:32:52 -0800

    add functions

--- flask_ftscursor/__init__.py
+++ flask_ftscursor/__init__.py
@@ -1,1 +1,2 @@
-from flask_ftscursor.flask_ftscursor import FTS
+from flask_ftscursor.flask_ftscursor import FTS, index, delete, search,
+search_results, add_to_index, remove_from_index, query_index
