3608d3d89f15c81848e36c3267df68b63375463a
anthony-aylward <aaylward@eng.ucsd.edu>
2019-01-02 20:32:43 -0800

    advance version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Flask-FTSCursor',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/anthony-aylward/flask-ftscursor',
     license='MIT',
     author='Anthony Aylward',
