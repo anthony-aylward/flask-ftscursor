4db56b603a301b6093d643514a7862a75a40d1d0
anthony-aylward <aaylward@eng.ucsd.edu>
2019-01-02 20:32:24 -0800

    refactor index, delete, and search functions into methods of the FTS class

--- flask_ftscursor/flask_ftscursor.py
+++ flask_ftscursor/flask_ftscursor.py
@@ -44,37 +44,35 @@ class FTS():
                 ctx.fts_db = self.connect()
             return ctx.fts_db
 
+    def index(self, table, id, searchable):
+        c = self.connection.cursor(factory=FTSCursor)
+        c.attach_source_db(current_app.config['FTS_SOURCE_DATABASE'])
+        c.index(table, id, searchable)
+        current_app.fts.connection.commit()
+        c.detach_source_db()
+
+    def delete(self, table, id):
+        c = self.connection.cursor(factory=FTSCursor)
+        c.delete(table, id)
+
+    def search(self, table, query, page, per_page):
+        c = self.connection.cursor(factory=FTSCursor)
+        hits = c.search(table, query)
+        return {
+            'hits': {
+                'total': len(hits),
+                'hits': tuple(
+                    {'_id': hit} for hit in hits[
+                        (page - 1) * per_page:page * per_page
+                    ]
+                )
+            }
+        }
+
 
 
 
 # Functions ====================================================================
-
-def fts_index(table, id, searchable):
-    c = current_app.fts.connection.cursor(factory=FTSCursor)
-    c.attach_source_db(current_app.config['FTS_SOURCE_DATABASE'])
-    c.index(table, id, searchable)
-    current_app.fts.connection.commit()
-    c.detach_source_db()
-
-
-def fts_delete(table, id):
-    c = current_app.fts.connection.cursor(factory=FTSCursor)
-    c.delete(table, id)
-
-
-def fts_search(table, query, page, per_page):
-    c = current_app.fts.connection.cursor(factory=FTSCursor)
-    hits = c.search(table, query)
-    return {
-        'hits': {
-            'total': len(hits),
-            'hits': tuple(
-                {'_id': hit} for hit in hits[
-                    (page - 1) * per_page:page * per_page
-                ]
-            )
-        }
-    }
 
 
 def search_results(search):
@@ -85,18 +83,27 @@ def search_results(search):
 def add_to_index(index, model):
     if not current_app.fts:
         return
-    fts_index(table=index, id=model.id, searchable=model.__searchable__)
+    current_app.fts.index(
+        table=index,
+        id=model.id,
+        searchable=model.__searchable__
+    )
     
 
 def remove_from_index(index, model):
     if not current_app.fts:
         return
-    fts_delete(table=index, id=model.id)
+    current_app.fts.delete(table=index, id=model.id)
 
 
 def query_index(index, query, page, per_page):
     if not current_app.fts:
         return [], 0
-    search = fts_search(table=index, query=query, page=page, per_page=per_page)
+    search = current_app.fts.search(
+        table=index,
+        query=query,
+        page=page,
+        per_page=per_page
+    )
     return search_results(search)
 
